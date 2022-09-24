209c371cabe2c1cd908d7393545980d5f9f21d39
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-24 16:33:03 +0000

    feat: add while loop to get guesses from user and provide feedback on guess

--- number_guess.sh
+++ number_guess.sh
@@ -17,3 +17,28 @@ else
   IFS=',' read USERNAME GAMES_PLAYED BEST_GAME <<< $USER
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
+
+NUMBER_OF_GUESSES=1
+
+while [[ $GUESS != $SECRET_NUMBER ]]
+do
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  elif [[ $GUESS > $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that guess again:"
+  elif [[ $GUESS < $SECRET_NUMBER ]]
+  then
+    echo "It's higher than that guess again:"
+  fi
+
+  read GUESS
+
+  NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
+done
+
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
